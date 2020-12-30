dd1ac6fc6fd800c3695e1cbf6b8d44bc468ea3f6
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-12-30 13:26:20 +0100

    Remove baker syscall

--- baker_syscall_windows.go
+++
@@ -1,9 +0,0 @@
-package baker
-
-import (
-	"syscall"
-)
-
-func bakerSyscall(trap, nargs, a1, a2, a3 uintptr) (r1, r2 uintptr, err syscall.Errno) {
-	return syscall.Syscall(trap, nargs, a1, a3, a3)
-}
